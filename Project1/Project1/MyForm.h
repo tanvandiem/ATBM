b9ca1352ba26e62bd194d8b9ac70068de9d2a6d2
tanvandiem <tanvandiem@gmail.com>
2024-04-03 23:15:57 +0700

    first commit

---
+++ Project1/Project1/MyForm.h
@@ -0,0 +1,345 @@
+﻿#pragma once
+
+namespace Project1 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::GroupBox^ groupBox1;
+	private: System::Windows::Forms::ProgressBar^ progressBar3;
+	private: System::Windows::Forms::ProgressBar^ progressBar2;
+	private: System::Windows::Forms::ProgressBar^ progressBar1;
+	private: System::Windows::Forms::GroupBox^ groupBox2;
+	private: System::Windows::Forms::ProgressBar^ progressBar5;
+	private: System::Windows::Forms::ProgressBar^ progressBar6;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label6;
+	private: System::Windows::Forms::Button^ button7;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::Label^ label7;
+	private: System::Windows::Forms::Label^ label8;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->groupBox1 = (gcnew System::Windows::Forms::GroupBox());
+			this->progressBar1 = (gcnew System::Windows::Forms::ProgressBar());
+			this->progressBar2 = (gcnew System::Windows::Forms::ProgressBar());
+			this->progressBar3 = (gcnew System::Windows::Forms::ProgressBar());
+			this->groupBox2 = (gcnew System::Windows::Forms::GroupBox());
+			this->progressBar5 = (gcnew System::Windows::Forms::ProgressBar());
+			this->progressBar6 = (gcnew System::Windows::Forms::ProgressBar());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label6 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->label7 = (gcnew System::Windows::Forms::Label());
+			this->label8 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->groupBox1->SuspendLayout();
+			this->groupBox2->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(8, 59);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(79, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Đường dẫn file:";
+			this->label1->Click += gcnew System::EventHandler(this, &MyForm::label1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(8, 110);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(96, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Đường dẫn file mã:";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(8, 139);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(115, 13);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Đường dẫn file giải mã:";
+			// 
+			// groupBox1
+			// 
+			this->groupBox1->Controls->Add(this->button7);
+			this->groupBox1->Controls->Add(this->button6);
+			this->groupBox1->Controls->Add(this->button5);
+			this->groupBox1->Controls->Add(this->button2);
+			this->groupBox1->Controls->Add(this->button1);
+			this->groupBox1->Controls->Add(this->progressBar3);
+			this->groupBox1->Controls->Add(this->progressBar2);
+			this->groupBox1->Controls->Add(this->progressBar1);
+			this->groupBox1->Controls->Add(this->label1);
+			this->groupBox1->Controls->Add(this->label3);
+			this->groupBox1->Controls->Add(this->label2);
+			this->groupBox1->Location = System::Drawing::Point(12, 12);
+			this->groupBox1->Name = L"groupBox1";
+			this->groupBox1->Size = System::Drawing::Size(385, 246);
+			this->groupBox1->TabIndex = 3;
+			this->groupBox1->TabStop = false;
+			this->groupBox1->Enter += gcnew System::EventHandler(this, &MyForm::groupBox1_Enter);
+			// 
+			// progressBar1
+			// 
+			this->progressBar1->Location = System::Drawing::Point(129, 59);
+			this->progressBar1->Name = L"progressBar1";
+			this->progressBar1->Size = System::Drawing::Size(203, 23);
+			this->progressBar1->TabIndex = 3;
+			// 
+			// progressBar2
+			// 
+			this->progressBar2->Location = System::Drawing::Point(129, 100);
+			this->progressBar2->Name = L"progressBar2";
+			this->progressBar2->Size = System::Drawing::Size(203, 23);
+			this->progressBar2->TabIndex = 4;
+			// 
+			// progressBar3
+			// 
+			this->progressBar3->Location = System::Drawing::Point(129, 139);
+			this->progressBar3->Name = L"progressBar3";
+			this->progressBar3->Size = System::Drawing::Size(203, 23);
+			this->progressBar3->TabIndex = 5;
+			// 
+			// groupBox2
+			// 
+			this->groupBox2->Controls->Add(this->button4);
+			this->groupBox2->Controls->Add(this->button3);
+			this->groupBox2->Controls->Add(this->progressBar5);
+			this->groupBox2->Controls->Add(this->progressBar6);
+			this->groupBox2->Controls->Add(this->label4);
+			this->groupBox2->Controls->Add(this->label6);
+			this->groupBox2->Location = System::Drawing::Point(418, 12);
+			this->groupBox2->Name = L"groupBox2";
+			this->groupBox2->Size = System::Drawing::Size(360, 246);
+			this->groupBox2->TabIndex = 4;
+			this->groupBox2->TabStop = false;
+			// 
+			// progressBar5
+			// 
+			this->progressBar5->Location = System::Drawing::Point(110, 110);
+			this->progressBar5->Name = L"progressBar5";
+			this->progressBar5->Size = System::Drawing::Size(203, 69);
+			this->progressBar5->TabIndex = 4;
+			// 
+			// progressBar6
+			// 
+			this->progressBar6->Location = System::Drawing::Point(110, 26);
+			this->progressBar6->Name = L"progressBar6";
+			this->progressBar6->Size = System::Drawing::Size(203, 68);
+			this->progressBar6->TabIndex = 3;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(25, 26);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(79, 13);
+			this->label4->TabIndex = 0;
+			this->label4->Text = L"Đường dẫn file:";
+			this->label4->Click += gcnew System::EventHandler(this, &MyForm::label4_Click);
+			// 
+			// label6
+			// 
+			this->label6->AutoSize = true;
+			this->label6->Location = System::Drawing::Point(25, 110);
+			this->label6->Name = L"label6";
+			this->label6->Size = System::Drawing::Size(79, 13);
+			this->label6->TabIndex = 1;
+			this->label6->Text = L"Đường dẫn file:";
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(129, 193);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 6;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Location = System::Drawing::Point(257, 193);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(75, 23);
+			this->button2->TabIndex = 7;
+			this->button2->Text = L"button2";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Location = System::Drawing::Point(110, 193);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(75, 23);
+			this->button3->TabIndex = 8;
+			this->button3->Text = L"button3";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Location = System::Drawing::Point(238, 193);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(75, 23);
+			this->button4->TabIndex = 9;
+			this->button4->Text = L"button4";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button5
+			// 
+			this->button5->Location = System::Drawing::Point(333, 59);
+			this->button5->Name = L"button5";
+			this->button5->Size = System::Drawing::Size(46, 23);
+			this->button5->TabIndex = 8;
+			this->button5->Text = L"button5";
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Location = System::Drawing::Point(333, 100);
+			this->button6->Name = L"button6";
+			this->button6->Size = System::Drawing::Size(46, 23);
+			this->button6->TabIndex = 9;
+			this->button6->Text = L"button6";
+			this->button6->UseVisualStyleBackColor = true;
+			// 
+			// button7
+			// 
+			this->button7->Location = System::Drawing::Point(333, 139);
+			this->button7->Name = L"button7";
+			this->button7->Size = System::Drawing::Size(46, 23);
+			this->button7->TabIndex = 10;
+			this->button7->Text = L"button7";
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(20, 278);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(79, 13);
+			this->label5->TabIndex = 5;
+			this->label5->Text = L"Đường dẫn file:";
+			// 
+			// label7
+			// 
+			this->label7->AutoSize = true;
+			this->label7->Location = System::Drawing::Point(266, 278);
+			this->label7->Name = L"label7";
+			this->label7->Size = System::Drawing::Size(79, 13);
+			this->label7->TabIndex = 6;
+			this->label7->Text = L"Đường dẫn file:";
+			// 
+			// label8
+			// 
+			this->label8->AutoSize = true;
+			this->label8->Location = System::Drawing::Point(524, 278);
+			this->label8->Name = L"label8";
+			this->label8->Size = System::Drawing::Size(79, 13);
+			this->label8->TabIndex = 7;
+			this->label8->Text = L"Đường dẫn file:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(345, 278);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(151, 20);
+			this->textBox1->TabIndex = 8;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::SystemColors::ActiveCaption;
+			this->ClientSize = System::Drawing::Size(828, 323);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label8);
+			this->Controls->Add(this->label7);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->groupBox2);
+			this->Controls->Add(this->groupBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->groupBox1->ResumeLayout(false);
+			this->groupBox1->PerformLayout();
+			this->groupBox2->ResumeLayout(false);
+			this->groupBox2->PerformLayout();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void groupBox1_Enter(System::Object^ sender, System::EventArgs^ e) {
+	}
+private: System::Void label4_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
